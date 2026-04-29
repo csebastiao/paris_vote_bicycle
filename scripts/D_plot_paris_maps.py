ba74ae5d9e1b57e59bd967f4f5391ed323b2ad4e
Clement Sebastiao <clement.sebastiao@gmail.com>
2026-04-29 10:25:15 +0200

    Add all map and scatter plots

--- scripts/D_plot_paris_maps.py
+++ scripts/D_plot_paris_maps.py
@@ -7,30 +7,34 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 
 
-FOLDER_DATA = "./data/processed/paris_official_data/"
+FOLDER_DATA = "./data/processed/"
 FOLDERPLOT = "./plots/"
 PLOT_COLS = {
-    "median_income": "Greens",
+    "median_income": "RdYlGn",
     "share_commuter_cyclist": "Greens",
     "share_commuter_driver": "Reds",
     "Left_wing_share": "Reds",
     "Right_wing_share": "Blues",
     "ratio_LR": "bwr_r",
+    "length_accomplished_share": "RdYlGn",
+    "length_before_2021": "RdYlGn",
+    "length_before_2021_norm": "RdYlGn",
+    "length_planned": "RdYlGn",
+    "length_planned_norm": "RdYlGn",
 }
 DPI = 300
 
 
+# TODO make json file for parameters
 def main():
-    gdf = gpd.read_file(FOLDER_DATA + "paris_vote_arr_2020.gpkg")
+    gdf = gpd.read_file(FOLDER_DATA + "paris_vote_arr_2020_bikenet.gpkg")
     for column_name, cmap in PLOT_COLS.items():
-        fig, ax = plt.subplots(layout="constrained")
+        fig, ax = plt.subplots(figsize=[11.69, 8.27], layout="constrained")
         if column_name == "ratio_LR":
             kwargs = {"vmin": -0.6, "vmax": 0.6}
         else:
             kwargs = {}
-        gdf.plot(
-            ax=ax, column=column_name, cmap=cmap, legend=True, linewidth=2, **kwargs
-        )
+        gdf.plot(ax=ax, column=column_name, cmap=cmap, legend=True, **kwargs)
         ax.axis("off")
         fig.savefig(FOLDERPLOT + "Paris_" + column_name + ".png", dpi=DPI)
 
---
+++ scripts/E_plot_scatterplot_votes_delays.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Plot results.
+"""
+
+import json
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import statsmodels.api as sm
+
+FOLDEROOTS = "./data/processed/"
+FOLDERPLOT = "./plots/"
+
+
+# TODO add OLS results R2, slope, and p-value on the scatterplot?
+def main():
+    with open("./scripts/E_plot_scatterplot_votes_delays.json", "r") as f:
+        plot_params = json.load(f)
+    for key in plot_params["rcparams"]:
+        mpl.rcParams[key] = plot_params["rcparams"][key]
+    gdf_vote = gpd.read_file(FOLDEROOTS + "paris_vote_arr_2020_bikenet.gpkg")
+    mean_acc = gdf_vote["length_accomplished_share"].mean()
+    for idx, column in enumerate(plot_params["column"]):
+        # Estimate linear regression
+        model = sm.OLS(
+            gdf_vote["length_accomplished_share"],
+            sm.add_constant(gdf_vote[column].values),
+        ).fit()
+        # Plot arrondissements
+        fig, ax = plt.subplots(figsize=plot_params["figsize"])
+        ax.scatter(
+            gdf_vote[column],
+            gdf_vote["length_accomplished_share"],
+            **{
+                key: val[idx]
+                for key, val in plot_params.items()
+                if key
+                not in [
+                    "figsize",
+                    "rcparams",
+                    "column",
+                    "xlabel",
+                    "xlim",
+                ]
+            },
+            label="Arrondissements",
+            zorder=2,
+        )
+        # Add expected value lines
+        ax.plot(
+            [-1, 99999],
+            [mean_acc, mean_acc],
+            linestyle="dashed",
+            color="#E1E1E1",
+            zorder=0,
+            label="Expected value",
+        )
+        if column != "ratio_LR":
+            mean_share = gdf_vote[column].mean()
+        else:
+            mean_share = 0
+        ax.plot(
+            [mean_share, mean_share],
+            [0, 1],
+            linestyle="dashed",
+            color="#E1E1E1",
+            zorder=0,
+        )
+        xx = np.linspace(
+            gdf_vote[column].min() - 0.05, gdf_vote[column].max() + 0.05, num=100
+        )
+        ax.plot(
+            xx,
+            model.params["x1"] * xx + model.params["const"],
+            color="#000000",
+            label="Linear regression",
+            linewidth=2,
+            zorder=1,
+        )
+        ax.set_ylabel("Share of bicycle lanes accomplished")
+        ax.set_xlabel(plot_params["xlabel"][idx])
+        ax.set_xlim(plot_params["xlim"][idx])
+        ax.set_ylim([0, 1])
+        ax.legend()
+        filename = f"scatterplot_delays_{column}"
+        fig.savefig(FOLDERPLOT + filename + ".png")
+
+
+if __name__ == "__main__":
+    main()
